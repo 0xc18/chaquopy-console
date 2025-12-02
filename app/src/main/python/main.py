fb0c294e2abb5aa7d7dc872923f49b308bca40c3
SAKIB-SALIM <144510317+SAKIB-SALIM@users.noreply.github.com>
2025-12-02 19:06:16 +0600

    tqdm test

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -1,11 +1,14 @@
 from six.moves import input
-
+from tqdm import tqdm
+from time import sleep
 
 def main():
     print("Enter your name, or an empty line to exit.")
     while True:
         try:
             name = input()
+            for i in tqdm(range(10)):
+                sleep(1)
         except EOFError:
             break
         if not name:
